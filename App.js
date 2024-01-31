6dba2cd03895ee845bbe246d2c1b7096b2e5e38f
Gavin Garzia <ggarzia@mines.edu>
2024-01-30 20:16:08 -0700

    tested multiple views

--- App.js
+++ App.js
@@ -4,8 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.container2}>
+        <Text style = {styles.text}>Test</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +16,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  container2: {
+    backgroundColor: '#002300',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
   },
 });
